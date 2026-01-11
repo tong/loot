6c303449f0607b05d44d8a0ab1a4844059a83191
tong <tong@disktree.net>
2026-01-11 18:54:49 +0100

    improve repl

--- server/repl.js
+++ server/repl.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import process from 'process';
+import * as readline from 'readline';
 import Client from './client.js';
 
 /*
@@ -36,76 +38,106 @@ function getTarget(id, clients) {
 }
 
 function startRepl(clients) {
-  process.stdin.on('data', data => {
-    const input = data.toString().trim();
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: "anal < "
+  });
+  rl.prompt();
+  rl.on("line", (line) => {
+    const input = line.trim();
+    if (!input) {
+      rl.prompt();
+      return;
+    }
     const parts = input.split(/\s+/);
     const command = parts[0];
     const target = parts[1];
     let eventType = '';
     let payload = '';
     if (command === "execute") {
-      payload = parts.splice(2).join(' ');
+      payload = parts.slice(2).join(' ');
     } else if (command === "subscribe" || command === "unsubscribe") {
       eventType = parts[2];
     }
-
-    if (command === "clients") {
-      if (clients.size === 0) {
-        console.info("0 clients connected");
-        return;
-      }
-      for (const client of clients.values()) {
-        console.info(` - ${client.getInfo()}`);
-      }
-      return;
-    }
-    if (command === "subscribe" && target && eventType) {
-      if (target === "all") {
-        Client.broadcast(clients, "subscribe", eventType);
-      } else {
-        const client = getTarget(target, clients);
-        if (client) {
-          console.info(`Subscribing client=${client.id}:event:${eventType}`);
-          client.send('subscribe', eventType);
+    switch (command) {
+      case "clients":
+        if (clients.size === 0) {
+          console.info("0 clients connected");
+        } else {
+          for (const client of clients.values()) {
+            console.info(` - ${client.getInfo()}`);
+          }
         }
-      }
-      return;
-    }
-    if (command === "unsubscribe" && target && eventType) {
-      if (target === "all") {
-        console.info(`Unsubscribing ALL from event=${eventType}`);
-        Client.broadcast(clients, "unsubscribe", eventType);
-      } else {
-        const client = getTarget(target, clients);
-        if (client) {
-          console.info(`Unsubscribing client=${client.id}:event=${eventType}`);
-          client.send('unsubscribe', eventType);
+        break;
+      case "subscribe":
+      case "sub":
+        if (!target || !eventType) {
+          console.warn("Usage: subscribe <id|all> <event>");
+          break;
         }
-      }
-      return;
-    }
-    if (command === "execute" && target && payload) {
-      if (target === "all") {
-        console.info(`Executing on ALL clients: ${payload}`);
-        Client.broadcast(clients, 'execute', payload);
-      } else {
-        const client = getTarget(target, clients);
-        if (client) {
-          console.info(`Executing client=${client.id}:code=${payload}`);
-          client.send('execute', payload);
+        if (target === "all") {
+          Client.broadcast(clients, "subscribe", eventType);
+        } else {
+          const client = getTarget(target, clients);
+          if (client) {
+            console.info(`Subscribing client=${client.id}:event:${eventType}`);
+            client.send('subscribe', eventType);
+          }
         }
-      }
-      return;
+        break;
+      case "unsubscribe":
+      case "unsub":
+        if (!target || !eventType) {
+          console.warn("Usage: unsubscribe <id|all> <event>");
+          break;
+        }
+        if (target === "all") {
+          // console.info(`Unsubscribing ALL from event=${eventType}`);
+          Client.broadcast(clients, "unsubscribe", eventType);
+        } else {
+          const client = getTarget(target, clients);
+          if (client) {
+            console.info(`Unsubscribing client=${client.id}:event=${eventType}`);
+            client.send('unsubscribe', eventType);
+          }
+        }
+        break;
+      case "execute":
+      case "exe":
+        console.log(target, payload);
+        if (!target || !payload) {
+          console.warn("Usage: execute <id|all> <js>");
+          break;
+        }
+        if (target === "all") {
+          console.info(`Executing on ALL clients: ${payload}`);
+          Client.broadcast(clients, 'execute', payload);
+        } else {
+          const client = getTarget(target, clients);
+          if (client) {
+            console.info(`Executing client=${client.id}:code=${payload}`);
+            client.send('execute', payload);
+          }
+        }
+        break;
+      case "help":
+      case "h":
+        console.log('  clients       - List all connected clients');
+        console.log('  execute <id|all> <js>  - Run js code on client(s)');
+        console.log('  subscribe <id|all> <eventType> - Subscribe client(s) to a event');
+        console.log('  unsubscribe <id|all> <eventType> - Unsubscribe client(s) from event');
+        break;
+      case "exit":
+      case "quit":
+        process.exit(0);
+      default:
+        console.log(`Unknown command [${command}] target=${target} payload=${payload}`);
     }
-    if (command === 'help') {
-      console.log('  clients       - List all connected clients');
-      console.log('  execute <id|all> <js>  - Run js code on client(s)');
-      console.log('  subscribe <id|all> <eventType> - Subscribe client(s) to a event');
-      console.log('  unsubscribe <id|all> <eventType> - Unsubscribe client(s) from event');
-      //...
-      return;
-    }
-    console.log(`Unknown command [${command}] target=${target} payload=${payload}`);
+    rl.prompt();
+  }).on("close", () => {
+    console.log("Exiting REPL");
+    process.exit(0);
   });
 }
 
